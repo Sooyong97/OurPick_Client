9552323af4645ac3f8eaa92adcaa6e7fae6f23c7
Sooyong Yoon <ybh6703@naver.com>
2025-04-24 02:32:44 +0900

    fix: config 파일 수정정

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        ws: true,
+        logLevel: 'debug'
+      }
+    }
   }
-}) 
+})
